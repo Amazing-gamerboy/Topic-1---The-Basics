584afd340ff9e2e05b195bc654b29a5f9cd57dea
339720229 <339720229@2260I-L29-005>
2024-09-05 09:29:12 -0400

    Add project files.

---
+++ Topic 1 - The Basics/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic_1___The_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ewan R.;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I will write on the next line");
+             Console.WriteLine("PRESS ENTER To Contine");
+         Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Hello world");
+            Console.Write("What is different");
+        }
+    }
+}             
